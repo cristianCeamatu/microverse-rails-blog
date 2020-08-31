15efa9ead0535f2e35d295443932561382347484
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-08-31 18:40:19 +0300

    Add Edit method and view New method and view Form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,47 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
   def show
     @article = Article.find(params[:id])
-    
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    flash.notice = "Article `#{@article.title}` was created! Congratulations!"
+
+    redirect_to @article
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
   
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article `#{@article.title}` was updated! Congratulations!"
+
+    redirect_to @article
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    flash.notice = "Article `#{@article.title}` was deleted! You will never see it again!"
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
