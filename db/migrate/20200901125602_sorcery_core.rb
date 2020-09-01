1072e51c33a9868d1696793f3a08d4deb36f5c66
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-09-01 16:36:14 +0300

    Add basic authentication

--- db/migrate/20200901125602_sorcery_core.rb
+++ db/migrate/20200901125602_sorcery_core.rb
@@ -1,6 +1,7 @@
 class SorceryCore < ActiveRecord::Migration[6.0]
   def change
     create_table :authors do |t|
+      t.string :username,         null: false
       t.string :email,            null: false
       t.string :crypted_password
       t.string :salt
